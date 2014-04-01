b7848dd9e40535d7e2413ac6e9d7e741016a6b79
Elliot Betancourt <elliot@elliotbetancourt.com>
2014-03-31 22:28:16 -0400

    add apache restart to vhosts recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,13 +8,19 @@
 #
 
 
-template "/etc/sudoers" do
-  source "sudoers.erb"
-  mode 0440
-  owner "root"
-  group "root"
-  variables({
-     :sudoers_groups => node[:authorization][:sudo][:groups],
-     :sudoers_users => node[:authorization][:sudo][:users]
-  })
+node[:vhost_hd][:vhosts].each do |vhost|
+    template "/etc/apache2/sites-available/#{vhost}" do
+      source "vhost.conf.erb"
+      mode 0440
+      owner "root"
+      group "root"
+      variables({
+         :vdomain => vhost
+      })
+    end
 end
+
+execute "service apache2 restart" do
+  command "service apache2 restart"
+end
+
